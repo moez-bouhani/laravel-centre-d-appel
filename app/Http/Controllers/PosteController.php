27e0728eebe588b028a4fc070d305f3f6d97e30b
Moez Bouhani <moez.bouhani@pinkswan.ch>
2023-03-30 11:17:00 +0000

    add number poste

--- app/Http/Controllers/PosteController.php
+++ app/Http/Controllers/PosteController.php
@@ -3,12 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Numero;
+use App\Poste;
 class PosteController extends Controller
 {
     public function store(Request $request)
     {
-        $Numero= Numero::create([
+        $Numero= Poste::create([
             'user_id'=>$request['user_id'],
             'num'=>$request['num']  
         ]);
